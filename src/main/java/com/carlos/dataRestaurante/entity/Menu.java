29136e2afd9f09dda6abbc7d0f5b4a23a4ca419f
CarlosDev <-c>
2022-01-03 15:19:02 -0600

    Se crean las clases de las tablas menu y tipo restaurante

---
+++ src/main/java/com/carlos/dataRestaurante/entity/Menu.java
@@ -0,0 +1,16 @@
+package com.carlos.dataRestaurante.entity;
+
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * Clase que representa la tabla de mysql menu y sus valores
+ * */
+public class Menu {
+
+    private int idMenu;
+    private String clave;
+    private String descripcion;
+
+}
---
+++ src/main/java/com/carlos/dataRestaurante/entity/Restaurante.java
@@ -0,0 +1,16 @@
+package com.carlos.dataRestaurante.entity;
+
+public class Restaurante    {
+
+    /**
+     * Atributos de la clase
+     * */
+
+    private int idRestaurante;
+    private String nombre;
+    private String descripcion;
+    private String slogan;
+    private TipoRestaurante tipoRestaurante;
+    private Menu menu;
+
+}
